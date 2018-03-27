8f36b81c9f03f7f79e4b87ad9189bff25c96c6b3
Dhaval Patel <pateldhaval762@gmail.com>
2018-03-26 23:32:56 -0400

    angular routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -18,7 +19,11 @@ import { GameComponent } from './game/game.component';
   ],
   imports: [
     BrowserModule,
-    
+     RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: 'game', component: GameComponent },
+      { path: '', redirectTo: '/home', pathMatch: 'full'}
+  ])   
   ],
   providers: [],
   bootstrap: [AppComponent]
