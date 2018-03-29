d79426219f72ad6d26e4508924cc92e7214130ef
Dhaval Patel <pateldhaval762@gmail.com>
2018-03-28 22:59:42 -0400

    class adjusment

--- src/app/models/game.ts
+++ src/app/models/game.ts
@@ -6,8 +6,8 @@ export class Game {
     ];
     Players: User[] = [
         { Name: 'Dk'},
-        { Name: 'Leonal'},
-        { Name: 'KP' }
+        { Name: 'KP'},
+        { Name: 'Leonal' }
     ];
     Picture: string = 'https://media3.s-nbcnews.com/j/msnbc/components/video/201803/tdy_news_welker_trump_180315_1920x1080.nbcnews-ux-1080-600.jpg';
     PlayedQuotes: Quote[] = [
