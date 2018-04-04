7a762655c834974597ef832b54b3c01dc0abe5fa
Dhaval <pateldhaval762@gmail.com>
2018-04-04 14:40:05 -0400

    Start on server

--- server/index.js
+++ server/index.js
@@ -5,9 +5,12 @@ const servername = "localhost";
 const port = 8080;
 
 const simple = require('./simpleController');
+const game = require('./game/controller');
 
 // respond with "hello world" when a GET request is made to the homepage
-app.use('/simple' ,simple).listen(port);
-
+app
+    .use('/simple' ,simple).listen(port)
+    .use('/game', game)
+    .listen(port);
 console.log("running on http://" +servername + ":" + port)
   
