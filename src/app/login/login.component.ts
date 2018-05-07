29bda42450358d468e4e703a84d428ba7fd0f5ee
Unknown <pateldhaval762@gmail.com>
2018-05-07 14:05:37 -0400

    Login with facebook

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { GameService } from '../services/game.service';
 
 declare var googleyolo: any;
+declare var FB: any;
+declare var window: any;
 
 @Component({
   selector: 'app-login',
@@ -11,6 +13,38 @@ declare var googleyolo: any;
 export class LoginComponent implements OnInit {
 
   constructor(private _Game: GameService) {
+      window.fbAsyncInit = () => {
+          FB.init({
+            appId   : '188596891769592',
+            cookie  : 'true',
+            xfbml   : 'true',
+            version : 'v2.12',
+          });
+
+          FB.AppEvents.logPageView();
+
+      };
+    (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = <HTMLScriptElement>d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_us/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));   
+   }
+
+  ngOnInit() {
+  }
+
+  login(name: string, password: string){
+    this._Game.login(name, password);
+  }
+  fblogin(){
+      FB.login((credentials:any)=>{
+          console.log(credentials);
+      })
+  }
+  googlelogin(){
     googleyolo.hint({
       supportedAuthMethods: [
           "https://accounts.google.com"
@@ -22,16 +56,9 @@ export class LoginComponent implements OnInit {
         }
       ]
     }).then((credentials: any) =>{
-      _Game.oAuthLogin(credentials.displayName, credentials.idToken, credentials.profilePicture);
+      this._Game.oAuthLogin(credentials.displayName, credentials.idToken, credentials.profilePicture);
       console.log(credentials);
     })
-   }
-
-  ngOnInit() {
-  }
-
-  login(name: string, password: string){
-    this._Game.login(name, password);
   }
 
 }
